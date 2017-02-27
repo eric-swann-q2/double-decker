9b69d7b6ff4a23177d0f6c2294e4182168eab961
Eric Swann <eric.swann@q2ebanking.com>
2017-02-26 20:36:12 -0600

    Added unit tests for message factory

--- test/message-factory-spec.ts
+++ test/message-factory-spec.ts
@@ -11,8 +11,8 @@ chai.use(chaiDateTime);
 
 describe('When creating an action', () => {
 
-  let factory:IMessageFactory = new MessageFactory();
-   const action = factory.CreateAction("TestAction", {test:"test", thingy:"thingy"});
+  let factory: IMessageFactory = new MessageFactory();
+  const action = factory.CreateAction("TestAction", { test: "testAction", thingy: "thingy" });
 
   it('Should have Action category', () => {
     expect(action.category).to.equal(Category.Action);
@@ -29,5 +29,36 @@ describe('When creating an action', () => {
     expect(action.timestamp).to.be.beforeTime(new Date());
   });
 
+  it('Should have valid data', () => {
+    expect(action.data.test).to.equal("testAction");
+    expect(action.data.thingy).to.equal("thingy");
+  });
+
+});
+
+describe('When creating an event', () => {
+
+  let factory: IMessageFactory = new MessageFactory();
+  const event = factory.CreateEvent("TestEvent", { test: "testEvent", thingy: "thingy" });
+
+  it('Should have Event category', () => {
+    expect(event.category).to.equal(Category.Event);
+  });
+
+  it('Should have an ID', () => {
+    expect(event.id).to.not.be.empty;
+  });
+
+  it('Should have a valid timestamp', () => {
+    expect(event.timestamp).to.not.be.null;
+    expect(event.timestamp).to.be.afterDate(new Date(2017, 1, 1));
+    expect(event.timestamp).to.equalDate(new Date());
+    expect(event.timestamp).to.be.beforeTime(new Date());
+  });
+
+  it('Should have valid data', () => {
+    expect(event.data.test).to.equal("testEvent");
+    expect(event.data.thingy).to.equal("thingy");
+  });
 
 });
